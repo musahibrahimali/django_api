6873a46f4ab20a08e728f654e4c4baff7d68e52c
Musah Ibrahim Ali <musahibrahimali@gmail.com>
2022-03-14 23:29:47 +0000

    testing client

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,3 +1,9 @@
 import requests
 
-endpoint = 'http://'
+# create an endpoint to jsonplaceholder
+url = 'https://jsonplaceholder.typicode.com/posts'
+
+# create a request to get the post from url
+response = requests.get(url)
+# print the response
+print(response.text)
